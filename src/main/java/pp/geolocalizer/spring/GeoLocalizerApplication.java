311be977305784c54ebc731500ac64254fe25bb3
PawelPruchniak <pw.pruchniak@gmail.com>
2021-09-18 12:30:27 +0200

    Classes refacor

---
+++ src/main/java/pp/geolocalizer/spring/GeoLocalizerApplication.java
@@ -0,0 +1,12 @@
+package pp.geolocalizer.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoLocalizerApplication {
+
+    public static void main( String... args ) {
+        SpringApplication.run( GeoLocalizerApplication.class, args );
+    }
+}
