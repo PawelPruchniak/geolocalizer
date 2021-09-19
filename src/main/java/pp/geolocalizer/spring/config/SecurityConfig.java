9aef089b5d0c10fa23ea87d9a7b78aed6a3938ff
PawelPruchniak <pw.pruchniak@gmail.com>
2021-09-19 13:53:58 +0200

    Turned csrf off

--- src/main/java/pp/geolocalizer/spring/config/SecurityConfig.java
+++ src/main/java/pp/geolocalizer/spring/config/SecurityConfig.java
@@ -1,39 +1,23 @@
 package pp.geolocalizer.spring.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure( AuthenticationManagerBuilder aAuth ) throws Exception {
-
-        aAuth
-                .inMemoryAuthentication()
-                .withUser( "user" )
-                .password( "{noop}user" )
-                .roles( "USER" )
-                .and()
-                .withUser( "admin" )
-                .password( "{noop}admin" )
-                .roles( "USER", "ADMIN" );
-    }
-
-    @Override
-    protected void configure( HttpSecurity aHttp ) throws Exception {
-
-        aHttp.httpBasic()
-                .and()
+    protected void configure( HttpSecurity http ) throws Exception {
+        http
                 .authorizeRequests()
-                .antMatchers( HttpMethod.POST, "/device" ).hasRole( "ADMIN" )
+                .anyRequest()
+                .authenticated()
                 .and()
-                .csrf()
-                .disable();
+                    .formLogin()
+                .and()
+                    .httpBasic()
+                .and()
+                    .csrf().disable();
     }
 }
